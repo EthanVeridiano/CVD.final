ac9668f27658dd2cb96daadf732c5f4fc9255f54
Doki-Doki-Doki <ryle_andrei_cabudol@dlsl.edu.ph>
2024-01-18 13:43:20 +0800

    completed(sana)

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const burgir = document.querySelector(".burgir__menu")
 const hNav = document.querySelector(".header__nav")
-
 burgir.addEventListener("click", () => {
     burgir.classList.toggle("open");
     hNav.classList.toggle("open");
+})
+const navBtn = document.querySelector(".updates__btn")
+navBtn.addEventListener("click", () => {
+    navBtn.classList.toggle("open");
 })
